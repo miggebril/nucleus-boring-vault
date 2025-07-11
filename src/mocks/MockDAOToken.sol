c400cb3289f2017f00970f0deae9d43827d81b7e
Mig Gebril <mig@tanius.net>
2025-07-11 10:24:37 -0400

    lint

--- src/mocks/MockDAOToken.sol
+++ src/mocks/MockDAOToken.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.21;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Mock is ERC20 {
-    uint256 public maxAmountAllowed = 100000000000000000000;
+    uint256 public maxAmountAllowed = 100_000_000_000_000_000_000;
 
-    constructor() ERC20("Mock XDAO", "MXDAO") {}
+    constructor() ERC20("Mock XDAO", "MXDAO") { }
 
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
